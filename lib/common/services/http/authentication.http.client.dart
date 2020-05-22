4755919c9adc10c1626c6ac99ebee04c616f4529
Marc van Leeuwen <marc2799015@gmail.com>
2020-05-22 16:47:15 +0200

    implement jwt login

--- lib/common/services/http/authentication.http.client.dart
+++ lib/common/services/http/authentication.http.client.dart
@@ -30,4 +30,18 @@ class AuthenticationHttpClient {
       throw Exception(json.decode(response.body));
     }
   }
+
+  Future<void> jwtLogin(String token) async {
+    final response = await http.post(
+        ConstantsUtils.SERVER_ADDRESS + '/authentication',
+        body: {"strategy": 'jwt', "accessToken": token});
+    if (response.statusCode < 400) {
+      // parse the JSON and store only the 'data' array in the iterable
+      String token = json.decode(response.body)["accessToken"];
+      // loop over the parsed JSON and create a new list for each index
+      AuthUtils.saveToken(token);
+    } else {
+      throw Exception(json.decode(response.body));
+    }
+  }
 }
